34635046b43b2f4bfdb48502480ded96c7712da5
Administrator <Administrator@F025>
2014-07-20 21:32:09 +0800

    nihao wojiao zangxiaohui

--- CRM/src/cn/sdut/persistence/dao/impl/Lg02DaoImpl.java
+++ CRM/src/cn/sdut/persistence/dao/impl/Lg02DaoImpl.java
@@ -1,6 +1,7 @@
 package cn.sdut.persistence.dao.impl;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
@@ -11,6 +12,74 @@ import cn.sdut.persistence.support.HibernatePageDaoSupport;
 import cn.sdut.system.Tools;
 
 public class Lg02DaoImpl extends HibernatePageDaoSupport implements Lg02Dao {
+    private Map dto=null;
 
+    @Override
+    public List query() throws Exception
+    {
+        
+        /**
+         * 
+         */
+        Object lg2102=this.dto.get("lg2102");    //员工编号
+        Object lg0401=this.dto.get("lg0401");    //员工部门
+        Object lg0202=this.dto.get("lg0202");    //员工姓名
+        Object lg0207=this.dto.get("lg0207");    //员工状态
+        Object blg0208=this.dto.get("blg0208");    //入职时间 开始
+        Object elg0208=this.dto.get("elg0208");    //入职时间 开始
+        this.pars = new ArrayList();
+        /**
+         * 需要查到
+         * 员工编号，员工姓名，员工部门，员工状态，入职时间
+         */
+        this.hql = new StringBuilder()
+        .append("select new map(a.lg2102 as lg2102,c.lg2102 as lg2102,a.lg0202 as lg0202,d.lg0402 as lg0402,")
+        .append("               b.fname as cnlg0207,to_char(a.lg0208,'YYYY-MM-DD') as lg0208")
+        .append(")")
+        .append("  from lg02 a,syscode b,lg21 c,lg04 d")
+        .append(" where a.lg0401=d.lg0401")
+        .append("   and c.lg2101=a.lg2101")
+        .append("   and a.lg0207=b.fvalue")
+        .append("   and b.fcode='llg01'")
+        ;
+        //员工编号
+        if(this.checkVal(lg2102))
+        {
+            this.hql.append(" and c.lg2102=?");
+            this.pars.add(lg2102);
+        }
+        //员工姓名
+        if(this.checkVal(lg0401))
+        {
+            this.hql.append(" and a.lg0401=?");
+            this.pars.add(lg0401);
+        }
+        //员工姓名
+        if(this.checkVal(lg0202))
+        {
+            this.hql.append(" and a.lg0202 like ?");
+            this.pars.add("%"+lg0202+"%");
+        }
+        //员工状态
+        if(this.checkVal(lg0207))
+        {
+            this.hql.append(" and a.lg0207=?");
+            this.pars.add(lg0207);
+        }
+        //入职时间 开始
+        if(this.checkVal(blg0208))
+        {
+            this.hql.append(" and a.lg0208>=?");
+            this.pars.add(blg0208);
+        }
+        //入职时间 结束
+        if(this.checkVal(elg0208))
+        {
+            this.hql.append(" and a.elg0208<=?");
+            this.pars.add(elg0208);
+        }
+        this.hql.append(" order by a.lg2102");
+        return this.queryForList();
+    }
 	
 }
--- CRM/src/cn/sdut/test/Lg02DaoTest.java
+++ CRM/src/cn/sdut/test/Lg02DaoTest.java
@@ -1,29 +1,45 @@
 package cn.sdut.test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import cn.sdut.persistence.bean.Lg02;
-import cn.sdut.persistence.bean.Lg04;
 import cn.sdut.persistence.dao.interfaces.Lg02Dao;
-import cn.sdut.persistence.dao.interfaces.Lg04Dao;
 
-public class Lg02DaoTest {
-	private static ApplicationContext act = new FileSystemXmlApplicationContext(
-	"C:/Users/Administrator/git/CRM/CRM/WebRoot/WEB-INF/applicationContext.xml");
-	private static Lg02Dao dao = act.getBean("lg02dao", Lg02Dao.class);
-	private static Map dto = new HashMap();
+public class Lg02DaoTest
+{
+    private static ApplicationContext act = new FileSystemXmlApplicationContext(
+            "C:/Users/Administrator/git/CRM/CRM/WebRoot/WEB-INF/applicationContext.xml");
+    private static Lg02Dao dao = act.getBean("lg02dao", Lg02Dao.class);
+    private static Map dto = new HashMap();
 
-	public static void main(String[] args) {
-		dao.setMapDto(dto);
-		try {
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+    public static void main(String[] args)
+    {
+        dao.setMapDto(dto);
+        try
+        {
+            Lg02DaoTest.queryTest();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void queryTest() throws Exception
+    {
+        // Object lg2102=this.dto.get("lg2102"); //员工编号
+        // Object lg0401=this.dto.get("lg0401"); //员工部门
+        // Object lg0202=this.dto.get("lg0202"); //员工姓名
+        // Object lg0207=this.dto.get("lg0207"); //员工状态
+        // Object blg0208=this.dto.get("blg0208"); //入职时间 开始
+        // Object elg0208=this.dto.get("elg0208"); //入职时间 开始
+        // dto.put("lg", "");
+        List list = dao.query();
+        System.out.println(list);
+    }
+
 }
