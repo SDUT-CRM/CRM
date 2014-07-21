dc4f79bb27ad8c84b8b0114315dc779a1c6e4e2d
qinchuan <toposort@outlook.com>
2014-07-21 11:11:39 +0800

    sth lost?

---
+++ CRM/src/cn/sdut/services/A1010Services.java
@@ -0,0 +1,13 @@
+package cn.sdut.services;
+
+import java.util.List;
+import java.util.Map;
+
+public interface A1010Services {
+	public void setMapDto(Map dto);
+
+	public List query() throws Exception;
+
+	//public String getPageInfo(String url);
+
+}
---
+++ CRM/src/cn/sdut/services/impl/A1010ServicesImpl.java
@@ -0,0 +1,47 @@
+package cn.sdut.services.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.sdut.persistence.dao.interfaces.Lg05Dao;
+import cn.sdut.persistence.dao.interfaces.Lg06Dao;
+import cn.sdut.services.A1010Services;
+
+public class A1010ServicesImpl implements A1010Services {
+	private Lg05Dao lg05Dao = null;
+	private Lg06Dao lg06Dao = null;
+	private Map dto = null;
+
+	/*
+	 * @Override public String getPageInfo(String url) { return
+	 * this.lg05Dao.getPageInfo(url); }
+	 */
+
+	public Lg05Dao getLg05Dao() {
+		return lg05Dao;
+	}
+
+	public void setLg05Dao(Lg05Dao lg05Dao) {
+		this.lg05Dao = lg05Dao;
+	}
+
+	public Lg06Dao getLg06Dao() {
+		return lg06Dao;
+	}
+
+	public void setLg06Dao(Lg06Dao lg06Dao) {
+		this.lg06Dao = lg06Dao;
+	}
+
+	@Override
+	public List query() throws Exception {
+		return this.lg05Dao.query();
+	}
+
+	@Override
+	public void setMapDto(Map dto) {
+		this.dto = dto;
+		this.lg05Dao.setMapDto(dto);
+		this.lg06Dao.setMapDto(dto);
+	}
+}
