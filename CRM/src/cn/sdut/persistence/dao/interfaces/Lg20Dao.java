aa9dd7a962578e70d11e6e40dbc6f4ba47f12c88
qinchuan <toposort@outlook.com>
2014-07-21 09:22:51 +0800

    add lg22

---
+++ CRM/src/cn/sdut/persistence/dao/interfaces/Lg20Dao.java
@@ -0,0 +1,25 @@
+package cn.sdut.persistence.dao.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Lg20Dao {
+	
+	public void setMapDto(Map dto);
+	
+	//添加问题
+	public boolean add() throws Exception;
+	
+	//追加问题
+	public boolean append() throws Exception;
+	
+	//修改问题
+	public boolean modify() throws Exception;
+	
+	//查询问题
+	public List query() throws Exception;
+	
+	//删除问题
+	public boolean delete() throws Exception;
+	
+}
