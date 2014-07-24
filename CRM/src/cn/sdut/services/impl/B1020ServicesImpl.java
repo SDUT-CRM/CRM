0ff9bd88ef879bcd703909173755b13abb737814
qinchuan <toposort@outlook.com>
2014-07-24 14:39:06 +0800

    create B

---
+++ CRM/src/cn/sdut/services/impl/B1020ServicesImpl.java
@@ -0,0 +1,67 @@
+package cn.sdut.services.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import cn.sdut.persistence.dao.interfaces.Lg05Dao;
+import cn.sdut.persistence.dao.interfaces.Lg06Dao;
+import cn.sdut.services.A1010Services;
+
+public class B1020ServicesImpl implements A1010Services {
+	private Lg05Dao lg05Dao = null;
+	private Lg06Dao lg06Dao = null;
+	private Map dto = null;
+	
+	@Override
+	public Map getInstance() throws Exception{
+		return this.lg05Dao.getInstance();
+	}
+	
+	@Override
+	public String getPageInfo(String url) {
+		return this.lg05Dao.getPageInfo(url);
+	}
+
+	public Lg05Dao getLg05Dao() {
+		return lg05Dao;
+	}
+
+	public Map getDto() {
+		return dto;
+	}
+
+	public void setDto(Map dto) {
+		this.dto = dto;
+		this.lg05Dao.setMapDto(dto);
+		this.lg06Dao.setMapDto(dto);
+	}
+
+	public void setLg05Dao(Lg05Dao lg05Dao) {
+		this.lg05Dao = lg05Dao;
+	}
+
+	public Lg06Dao getLg06Dao() {
+		return lg06Dao;
+	}
+
+	public void setLg06Dao(Lg06Dao lg06Dao) {
+		this.lg06Dao = lg06Dao;
+	}
+
+	@Override
+	public List query() throws Exception {
+		return this.lg05Dao.queryForPage();
+	}
+
+	@Override
+	public void setMapDto(Map dto) {
+		this.dto = dto;
+		this.lg05Dao.setMapDto(dto);
+		this.lg06Dao.setMapDto(dto);
+	}
+
+	@Override
+	public boolean check() throws Exception {
+		return this.lg06Dao.check();
+	}
+}
--- CRM/src/cn/sdut/web/b1/impl/B1020Action.java
+++ CRM/src/cn/sdut/web/b1/impl/B1020Action.java
@@ -1,33 +1,27 @@
-package cn.sdut.web.a1.impl;
+package cn.sdut.web.b1.impl;
 
 import java.util.List;
 import java.util.Map;
 
+import cn.sdut.system.Tools;
+import cn.sdut.web.a1.A1010Support;
+
 import com.opensymphony.xwork2.ActionContext;
 
-import cn.sdut.system.Tools;
-import cn.sdut.web.a1.A1041Support;
-
-public class A1040Action extends A1041Support {
+public class B1020Action extends A1010Support {
 
 	@Override
 	public String execute() throws Exception {
 		try {
-			List rows = this.getA1041Services().queryById();
+			this.oclg0509 = Tools.getOptions("LG0509");
+			List rows = this.getA1010Services().query();
 			if (rows.size() > 0) {
 				this.rows = rows;
-				String pageinfo = this.getA1041Services().getPageInfo(
-						"a1042Action");
+				String pageinfo = this.getA1010Services().getPageInfo("a1010Action");
 				Map request = (Map) ActionContext.getContext().get("request");
 				request.put("pageinfo", pageinfo);
 			} else {
 				this.msg = "没有符合条件的数据";
-			}
-			boolean res = this.getA1041Services().add();
-			if (res == true) {
-				this.msg = "提问成功!";
-			} else {
-				this.msg = "提问失败!";
 			}
 		} catch (Exception e) {
 			this.msg = "网络故障";
