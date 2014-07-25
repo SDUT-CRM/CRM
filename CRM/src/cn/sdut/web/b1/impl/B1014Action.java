909847b504d0fcf6468907fd82ebc9effdc7e5d2
Administrator <Administrator@F025>
2014-07-25 15:45:16 +0800

    zxzz

---
+++ CRM/src/cn/sdut/web/b1/impl/B1014Action.java
@@ -0,0 +1,15 @@
+package cn.sdut.web.b1.impl;
+
+import cn.sdut.web.b1.B1010Support;
+
+public class B1014Action extends B1010Support
+{
+
+    @Override
+    public String execute() throws Exception
+    {
+        
+        return "main";
+    }
+
+}
